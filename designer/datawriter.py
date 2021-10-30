50cd8977ddb9e58b14739ce5aff9f8cd17b6ce8a
Deren Teo <hybrid.iii.209141514@gmail.com>
2021-10-30 21:15:37 +1000

    Changed DataWriter to write to specific tables
    To overcome where quantity name occurs multiple times

--- designer/datawriter.py
+++ designer/datawriter.py
@@ -2,18 +2,25 @@ import xlwings
 
 class DataWriter:
     ''''''
-    def __init__(self, filepath):
+    def __init__(self, filepath, exit_condition=200):
         ''''''
         self.wb = xlwings.Book(filepath)
         self.sh = self.wb.sheets[0]
+        self.exit_condition = exit_condition
 
-    def write(self, quantity, value, qcol='C', vcol='F', exit_condition=200):
+    # TODO: make this write per table
+    def write(self, quantity, value, table=0, qcol='C', vcol='F', tcol='D'):
         ''''''
         rownum = 8  # quantity names always start on row 8
 
+        # Specify a table to skip to
+        if table:
+            while (self.sh.range(f'{tcol}{rownum}').value != table):
+                rownum += 1
+
         # Search for the row with the specified quantity
         while (self.sh.range(f'{qcol}{rownum}').value != quantity):
-            if rownum > exit_condition:
+            if rownum > self.exit_condition:
                 raise ValueError(f'Quantity "{quantity}" not found')
             rownum += 1
 
